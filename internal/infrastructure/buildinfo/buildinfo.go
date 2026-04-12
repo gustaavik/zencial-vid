1275a31287ffb27e646b4ea0a6c03b78cfcbc4e9
gustaavik <gkhaavik@gmail.com>
2026-04-12 19:43:41 +0200

    Revert "fix: Update versioning logic in Makefile and Dockerfile for better tag resolution"
    This reverts commit c2011cfac9b1a763974ed6fd7542f476984286ec.

--- internal/infrastructure/buildinfo/buildinfo.go
+++ internal/infrastructure/buildinfo/buildinfo.go
@@ -1,29 +1,8 @@
 package buildinfo
 
-import "os"
-
 // Set at build time via -ldflags.
 var (
 	Version   = "dev"
 	Commit    = "unknown"
 	BuildTime = "unknown"
 )
-
-func init() {
-	sc := os.Getenv("SOURCE_COMMIT")
-	if sc == "" {
-		return
-	}
-
-	if Commit == "unknown" {
-		if len(sc) > 7 {
-			Commit = sc[:7]
-		} else {
-			Commit = sc
-		}
-	}
-
-	if Version == "dev" || Version == "0.0.0-unknown" {
-		Version = "0.0.0-" + Commit
-	}
-}
